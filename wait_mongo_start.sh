bb22906b351c793dff9a89cbb0ef631c4e9b9341
Scott Fredericksen <scott@innovationsdelivered.com>
2015-03-22 17:51:04 -0600

    put mongo_wait script back to the way it was

--- wait_mongo_start.sh
+++ wait_mongo_start.sh
@@ -7,15 +7,15 @@ polling_interval=30
 
 echo "waiting for mongo to start before launching App..."
 
-#wait until mongo is running in db_1 container (checked via netcat - nc)
-# until nc -z $DB_1_PORT_27017_TCP_ADDR $DB_1_PORT_27017_TCP_PORT
-# do
-# 	echo "waiting for $polling_interval seconds..."
-# 	sleep $polling_interval
-# done
+# wait until mongo is running in db_1 container (checked via netcat - nc)
+until nc -z $DB_1_PORT_27017_TCP_ADDR $DB_1_PORT_27017_TCP_PORT
+do
+	echo "waiting for $polling_interval seconds..."
+	sleep $polling_interval
+done
 
-echo "waiting for $polling_interval secnods..."
-sleep $polling_interval
+# echo "waiting for $polling_interval secnods..."
+# sleep $polling_interval
 
 #start nod App
 grunt
