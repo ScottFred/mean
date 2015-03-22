7ffa560f2f94423d3d3864d7b79bf07b1be2605a
Scott Fredericksen <scott@innovationsdelivered.com>
2015-03-22 15:15:18 -0600

    30 sec delay for mongoDB

--- wait_mongo_start.sh
+++ wait_mongo_start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-polling_interval=3
+polling_interval=30
 
 #optional, view db_1 container-related env vars
 #env | grep DB_1 | sort
@@ -8,11 +8,14 @@ polling_interval=3
 echo "waiting for mongo to start before launching App..."
 
 #wait until mongo is running in db_1 container (checked via netcat - nc)
-until nc -z $DB_1_PORT_27017_TCP_ADDR $DB_1_PORT_27017_TCP_PORT
-do
-	echo "waiting for $polling_interval seconds..."
-	sleep $polling_interval
-done
+# until nc -z $DB_1_PORT_27017_TCP_ADDR $DB_1_PORT_27017_TCP_PORT
+# do
+# 	echo "waiting for $polling_interval seconds..."
+# 	sleep $polling_interval
+# done
+
+echo "waiting for $polling_interval secnods..."
+sleep $polling_interval
 
 #start nod App
 grunt
